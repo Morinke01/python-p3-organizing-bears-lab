32cf3df7f0786b2766d2d63045d866d8e2d63eef
Morinke01 <ejulius250@gmail.com>
2023-12-09 10:09:16 +0300

    Assignment completed

---
+++ lib/insert.sql
@@ -0,0 +1,24 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 5, 'M', 'Brown', 'Friendly', TRUE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Rowdy', 3, 'M', 'Black', 'Playful', TRUE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Tabitha', 2, 'F', 'White', 'Calm', TRUE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Sergeant Brown', 7, 'M', 'Brown', 'Disciplined', TRUE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Melissa', 4, 'F', 'Golden', 'Energetic', TRUE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Grinch', 6, 'M', 'Green', 'Mischievous', TRUE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Wendy', 8, 'F', 'Brown', 'Gentle', TRUE);
+
+-- Inserting unnamed bear with a NULL name
+INSERT INTO bears (age, sex, color, temperament, alive) VALUES
+(1, 'F', 'Gray', 'Curious', TRUE);
--- lib/seed.sql
+++ lib/seed.sql
@@ -6,4 +6,48 @@ INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
 (5,"Melissa", 13, "F", "dark brown", "goofy", 1),
 (6,"Grinch", 2, "M", "Black", "Grinchy", 1),
 (7,"Wendy", 6, "F", "Blue", "naive", 1),
-(8,null, 20, "M", "black", "aggressive", 0);
+(8,NULL, 20, "M", "black", "aggressive", 0);
+
+
+select_all_bears_names_and_orders_in_alphabetical_order = """
+    SELECT
+        bears.name
+    FROM bears
+    ORDER BY bears.name COLLATE NOCASE;
+"""
+
+select_all_female_bears_return_name_and_age = """
+    SELECT
+        bears.name,
+        bears.age
+    FROM bears
+    WHERE sex="F";
+"""
+
+select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
+    SELECT
+        bears.name,
+        bears.age
+    FROM bears
+    WHERE alive=1
+    ORDER BY bears.age;
+"""
+
+select_oldest_bear_and_returns_name_and_age = """
+    SELECT
+        bears.name,
+        bears.age
+    FROM bears
+    ORDER BY bears.age DESC
+    LIMIT 1;
+"""
+
+select_youngest_bear_and_returns_name_and_age = """
+    SELECT
+        bears.name,
+        bears.age
+    FROM bears
+    ORDER BY bears.age
+    LIMIT 1;
+"""
+
